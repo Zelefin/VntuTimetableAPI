35f639ade257fb2ef28b966926109999dbe8738b
Zelefin <nagangoroh@gmail.com>
2024-04-18 22:33:23 +0300

    just type annotations

--- app/db_session.py
+++ app/db_session.py
@@ -1,12 +1,14 @@
 from typing import AsyncGenerator
 
-from configreader import load_config
+from sqlalchemy.orm import sessionmaker
+
+from configreader import load_config, Config
 from db.Repo import Repo
 from db.db import sa_sessionmaker
 
 
-config = load_config()
-session_factory = sa_sessionmaker(config.postgres)
+config: Config = load_config()
+session_factory: sessionmaker = sa_sessionmaker(config.postgres)
 
 
 async def get_session() -> AsyncGenerator[Repo, None]:
