db6c00de8693bda447bcd7138bc0d8b61a30e315
Zelefin <nagangoroh@gmail.com>
2024-04-19 21:52:19 +0300

    new methods

--- db/Repo.py
+++ db/Repo.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.models import Lesson, Group, Faculty
+from db.models import Lesson, Group, Faculty, Teacher
 
 
 class Repo:
@@ -24,8 +24,20 @@ class Repo:
     async def get_faculties(self) -> Sequence[Faculty]:
         return (await self.session.scalars(select(Faculty))).all()
 
+    async def get_faculty(self, faculty_id) -> Faculty | None:
+        return (await self.session.execute(select(Faculty).where(Faculty.id == faculty_id))).scalar_one_or_none()
+
     async def get_groups(self) -> Sequence[Group]:
         return (await self.session.scalars(select(Group))).all()
 
+    async def get_group(self, group_id: int) -> Group | None:
+        return (await self.session.execute(select(Group).where(Group.id == group_id))).scalar_one_or_none()
+
     async def get_groups_of_faculty(self, faculty_id: int) -> Sequence[Group]:
         return (await self.session.scalars(select(Group).where(Group.faculty_id == faculty_id))).all()
+
+    async def get_teacher_by_id(self, teacher_id: int) -> Teacher | None:
+        return (await self.session.execute(select(Teacher).where(Teacher.id == teacher_id))).scalar_one_or_none()
+
+    async def get_teacher_by_name(self, teacher_name: str) -> Teacher | None:
+        return (await self.session.execute(select(Teacher).where(Teacher.name == teacher_name))).scalar()
