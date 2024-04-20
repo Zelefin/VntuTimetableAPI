f874c8d0d2e0822fc247abb93c9fd4e74c08ab7f
Zelefin <nagangoroh@gmail.com>
2024-04-20 13:02:57 +0300

    added redis to env and docker

--- app/redis_session.py
+++ app/redis_session.py
@@ -3,7 +3,12 @@ from typing import AsyncGenerator
 from redis import asyncio as aioredis
 from redis.asyncio import Redis
 
+from configreader import Config, load_config
+
+
+config: Config = load_config()
+
 
 async def get_redis() -> AsyncGenerator[Redis, None]:
-    redis: Redis = aioredis.from_url("redis://localhost:6379/0")
+    redis: Redis = aioredis.from_url(f"redis://{config.redis.host}:{config.redis.port}/{config.redis.db}")
     yield redis
