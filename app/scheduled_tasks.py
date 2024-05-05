3286a6e31e553ff348e70f2a30825a1849050350
Zelefin <nagangoroh@gmail.com>
2024-05-05 23:43:13 +0300

    passing redis to each updating function

--- app/scheduled_tasks.py
+++ app/scheduled_tasks.py
@@ -1,6 +1,7 @@
 import logging
 
-from apscheduler.job import Job
+from apscheduler.job import Job  # type: ignore
+from redis.asyncio import Redis
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
@@ -14,12 +15,12 @@ from db.Repo import Repo
 
 
 async def update_faculties_table(
-    session_factory: async_sessionmaker[AsyncSession], retry_task: Job
+    session_factory: async_sessionmaker[AsyncSession], redis: Redis, retry_task: Job
 ) -> None:
     try:
         async with session_factory() as session:
             repo: Repo = Repo(session=session)
-            await update_faculties(repo=repo)
+            await update_faculties(repo=repo, redis=redis)
         retry_task.pause()
     except Exception as e:
         logging.info("Error while updating faculties table")
@@ -28,7 +29,7 @@ async def update_faculties_table(
 
 
 async def update_groups_table(
-    session_factory: async_sessionmaker[AsyncSession], retry_task: Job
+    session_factory: async_sessionmaker[AsyncSession], redis: Redis, retry_task: Job
 ) -> None:
     try:
         async with session_factory() as session:
@@ -36,7 +37,7 @@ async def update_groups_table(
             faculties: list[int] = [
                 faculty.id for faculty in await repo.get_faculties()
             ]
-            await update_groups(repo=repo, faculties=faculties)
+            await update_groups(repo=repo, redis=redis, faculties=faculties)
         retry_task.pause()
     except Exception as e:
         logging.info("Error while updating groups table")
@@ -44,34 +45,34 @@ async def update_groups_table(
         retry_task.resume()
 
 
-async def update_teachers_table(
-    session_factory: async_sessionmaker[AsyncSession], retry_task: Job
-) -> None:
-    try:
-        async with session_factory() as session:
-            repo: Repo = Repo(session=session)
-            await update_teachers(repo=repo)
-        retry_task.pause()
-    except Exception as e:
-        logging.info("Error while updating teachers table")
-        logging.exception(e)
-        retry_task.resume()
+# async def update_teachers_table(
+#     session_factory: async_sessionmaker[AsyncSession], retry_task: Job
+# ) -> None:
+#     try:
+#         async with session_factory() as session:
+#             repo: Repo = Repo(session=session)
+#             await update_teachers(repo=repo)
+#         retry_task.pause()
+#     except Exception as e:
+#         logging.info("Error while updating teachers table")
+#         logging.exception(e)
+#         retry_task.resume()
 
 
 async def update_groups_lessons_table(
-    session_factory: async_sessionmaker[AsyncSession], retry_task: Job
+    session_factory: async_sessionmaker[AsyncSession], redis: Redis, retry_task: Job
 ) -> None:
     try:
         async with session_factory() as session:
             repo: Repo = Repo(session=session)
             groups_ids: list[int] = [group.id for group in await repo.get_groups()]
-            await update_groups_lessons(repo=repo, groups_ids=groups_ids)
+            await update_groups_lessons(repo=repo, redis=redis, groups_ids=groups_ids)
         retry_task.pause()
     except IntegrityError:
         logging.info("Error while updating groups lessons table. Problem with teachers")
         await update_teachers(repo=repo)
         logging.info("Updated teachers")
-        await update_groups_lessons(repo=repo, groups_ids=groups_ids)
+        await update_groups_lessons(repo=repo, redis=redis, groups_ids=groups_ids)
         logging.info("Updated groups lessons")
     except Exception as e:
         logging.info(
