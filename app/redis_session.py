cb1d4da8d6886a07d8e88f7630a17a961fd2ea81
Zelefin <nagangoroh@gmail.com>
2024-05-08 20:59:12 +0300

    added dockstring; new name for config reader

--- app/redis_session.py
+++ app/redis_session.py
@@ -3,13 +3,14 @@ from typing import AsyncGenerator
 from redis import asyncio as aioredis
 from redis.asyncio import Redis
 
-from configreader import Config, load_config
+from config_reader import Config, load_config
 
 
 config: Config = load_config()
 
 
 async def get_redis() -> AsyncGenerator[Redis, None]:
+    """Function to get redis instance"""
     redis: Redis = aioredis.from_url(
         f"redis://{config.redis.host}:{config.redis.port}/{config.redis.db}"
     )
