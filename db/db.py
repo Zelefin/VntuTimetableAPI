b1dc78a9f1730c9d9f5771b9c00dc4790632ce3f
Zelefin <nagangoroh@gmail.com>
2024-05-08 20:50:07 +0300

    added dockstings

--- db/db.py
+++ db/db.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
-from configreader import Postgres
+from config_reader import Postgres
 
 
 def make_connection_string(db: Postgres, async_fallback: bool = False) -> str:
+    """Function to make a connection string to postgres database."""
     result = f"postgresql+asyncpg://{db.user}:{db.password}@{db.host}:{db.port}/{db.db}"
     if async_fallback:
         result += "?async_fallback=True"
@@ -11,6 +12,7 @@ def make_connection_string(db: Postgres, async_fallback: bool = False) -> str:
 
 
 def create_engine(connection_string: str, echo: bool = False):
+    """Function to create engine from connection string."""
     engine = create_async_engine(
         connection_string,
         query_cache_size=1200,
@@ -25,6 +27,7 @@ def create_engine(connection_string: str, echo: bool = False):
 def sa_sessionmaker(
     db: Postgres, echo: bool = False
 ) -> async_sessionmaker[AsyncSession]:
+    """Function to create sa session pool."""
     engine = create_engine(make_connection_string(db), echo=echo)
     session_pool = async_sessionmaker(
         bind=engine, expire_on_commit=False, autoflush=False, class_=AsyncSession
