19517db0a322088f72c3f2cd1b3ba7b6f54b412b
Zelefin <nagangoroh@gmail.com>
2024-05-08 20:57:54 +0300

    added dockstring

--- app/routes/teachers.py
+++ app/routes/teachers.py
@@ -12,6 +12,11 @@ teachers_router = APIRouter()
 
 @teachers_router.post("/v0/teachers")
 async def update_teachers_request(repo: Repo = Depends(get_session)) -> Dict:
+    """
+    Updates all teachers.
+    :param repo: db repo
+    :return: message (on success) or error (on failure)
+    """
     try:
         await update_teachers(repo=repo)
         return {"message": "Teachers updated"}
