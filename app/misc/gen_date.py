bb94e2ea434064a835bbef3192f4d6ce703894be
Zelefin <nagangoroh@gmail.com>
2024-08-12 09:28:56 +0300

    fix and refactor gen_weeks_dates function

--- app/misc/gen_date.py
+++ app/misc/gen_date.py
@@ -7,24 +7,18 @@ def gen_weeks_dates() -> list[dict[int, str]]:
     Each week is a dict with keys {day_num: date} like so: {0: "22.04", 1: "23.04" ...}
     :return: [1 week with dates, 2 week with dates]
     """
+    today = datetime.date.today()
 
-    today: datetime.date = datetime.date.today()
-    this_week_start: datetime.date = today - datetime.timedelta(
-        days=(today.weekday()) % 7
-    )
-    next_week_start: datetime.date = this_week_start + datetime.timedelta(days=7)
+    this_week_start = today - datetime.timedelta(days=today.weekday())
 
-    this_week_dates: dict[int, str] = {
-        i: (this_week_start + datetime.timedelta(days=i)).strftime("%d.%m")
-        for i in range(7)
-    }
-    next_week_dates: dict[int, str] = {
-        i: (next_week_start + datetime.timedelta(days=i)).strftime("%d.%m")
-        for i in range(7)
-    }
+    weeks = [
+        {
+            i: (this_week_start + datetime.timedelta(days=7 * week + i)).strftime(
+                "%d.%m"
+            )
+            for i in range(7)
+        }
+        for week in range(2)
+    ]
 
-    return (
-        [this_week_dates, next_week_dates]
-        if datetime.date.isocalendar(datetime.date.today())[1] % 2 == 0
-        else [next_week_dates, this_week_dates]
-    )
+    return weeks if today.isocalendar()[1] % 2 != 0 else weeks[::-1]
